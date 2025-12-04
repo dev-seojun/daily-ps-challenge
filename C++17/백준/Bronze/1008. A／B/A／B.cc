51c1e6f9fe9c8917cc78bec9d1129d9445cf9618
이서준 <86279096+dev-seojun@users.noreply.github.com>
2025-12-04 23:08:41 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+  double a, b;
+
+  std::cin >> a >> b;
+
+  std::cout << std::fixed;
+  std::cout << std::setprecision(10);
+
+  std::cout << a / b << std::endl;
+
+  return 0;
+}
