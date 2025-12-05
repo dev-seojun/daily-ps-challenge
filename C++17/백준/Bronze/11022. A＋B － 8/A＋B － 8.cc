f9cbb83772aee4075e1133b54cef2fc5443b7f73
이서준 <86279096+dev-seojun@users.noreply.github.com>
2025-12-05 23:10:57 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  int t;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::cin >> t;
+
+  for (int i = 1; i <= t; i++) {
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+  }
+
+  return 0;
+}
