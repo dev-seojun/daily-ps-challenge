212a4a8e0e1fd51e55c48634528b81989a1a9264
이서준 <86279096+dev-seojun@users.noreply.github.com>
2025-12-06 13:35:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  char a;
+  cin >> a;
+
+  cout << int(a) << "\n";
+
+  return 0;
+}
