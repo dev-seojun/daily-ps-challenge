29848a1a9ba01f51b33951094a2b713268241aae
이서준 <86279096+dev-seojun@users.noreply.github.com>
2025-12-07 16:35:53 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, m; cin >> n >> m;
+
+  vector<vector<int>> A(n, vector<int>(m));
+  vector<vector<int>> B(n, vector<int>(m));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> A[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> B[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << A[i][j] + B[i][j] << " ";
+    }
+
+    cout << "\n";
+  }
+
+  return 0;
+}
